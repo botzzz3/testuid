e524573aa72eeac480fb3269e60507c70fecc3f4
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-03-05 04:00:20 -0300

    Add files via upload

---
+++ src/charope.js
@@ -0,0 +1,21 @@
+const charope = (prefix, sender) => {
+	return `ApRoVeItE oS mEnUs✌️
+ 
+OLÁ, TUDO BEM? ESPERO QUE SIM♥️
+SOU O BOT DO GRUPO 
+BORA PRO COMANDO
+_______________________________
+ PARA TODOS OS COMANDO DIGITE "."
+ -----------------------------
+ ◻️.STICKER(converter imagem,gif em figu)
+ ◻️.tts(use o /tts para conseguir escutar)
+ ◻️POR ENQUANTO SO🤗
+╰────────────────────────
+  C0MANDO DE OUTRO BOT(manual)
+  ───────────────────────
+  /tts(PARA VER AS VEZES QUE TEM)
+  /dono(CRIADOR DO BOT)
+  '
+}
+
+exports.charope = charope
